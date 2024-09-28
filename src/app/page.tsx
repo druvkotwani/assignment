c03d2498a82cf5063b4f82fc2fcaf57b46c91891
druvkotwani <druvkotwani12@gmail.com>
2024-09-28 14:08:47 +0530

    lucide react installed + added data into page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,56 @@
 import Link from "next/link";
-import Dashboard from "./components/Dashboard";
+
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="bg-background min-h-screen font-sourceCodePro relative flex items-center justify-center">
-      <Dashboard />
+    <div className="bg-background min-h-screen font-sourceCodePro relative flex items-center flex-col md:flex-row justify-center gap-8 px-8 lg:px-24">
+      <div className="p-4 px-8 rounded border border-[#312f2f] flex flex-col items-center justify-center gap-8">
+        <ul className="flex flex-col gap-4 mt-4 text-[#ccc]">
+          <li>
+            {" "}
+            <span className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              ✦
+            </span>{" "}
+            Used Trading View&apos;s charting library
+          </li>
+          <li>
+            {" "}
+            <span className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              ✦
+            </span>{" "}
+            Used Tailwind CSS for styling
+          </li>
+        </ul>
 
+        <button className="px-6 py-3 bg-gradient-to-r from-teal-400  to-sky-600 rounded">
+          <Link href={"/"}>Dashboard 1</Link>
+          <ArrowRight className="w-5 h-5 inline-block ml-2" />
+        </button>
+      </div>
+      <div className="p-4 px-8 rounded border border-[#312f2f] flex items-center justify-center flex-col gap-8">
+        <ul className="flex flex-col gap-4 mt-4 text-[#ccc]">
+          <li>
+            {" "}
+            <span className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              ✦
+            </span>{" "}
+            Used Trading View&apos;s charting library
+          </li>
+          <li>
+            {" "}
+            <span className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              ✦
+            </span>{" "}
+            Used Tailwind CSS for styling
+          </li>
+        </ul>
+
+        <button className="px-6 py-3 bg-gradient-to-r from-green-300 to-pink-400 rounded">
+          <Link href={"/"}>Dashboard 2</Link>
+          <ArrowRight className="w-5 h-5 inline-block ml-2" />
+        </button>
+      </div>
       <footer className="text-[#ccc] absolute bottom-4 text-center flex w-full items-center justify-center text-lg">
         Assignment made with&nbsp;
         <span className="hover:scale-125 duration-300 transition-all ease-in-out transform">
